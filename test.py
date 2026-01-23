74734251d833e36c635e2975d6665752cffe0fe6
Richard Shelmerdine <richard@richardshelmerdine.com>
2026-01-23 21:09:45 +0000

    Upload

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from datetime import datetime
 
 actual = {
-    "top6": ["arsenal", "manchester city", "villa", "liverpool", "brentford", "newcastle", ],
+    "top6": ["arsenal", "manchester city", "villa", "liverpool", "manchester united", "chelsea" ],
     "bottom3": ["west ham", "burnley", "wolves"],
     "cups": {
         "fa cup":           {"winner": "placeholder", "runner_up": "placeholder"},
